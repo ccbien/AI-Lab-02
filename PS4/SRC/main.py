d9169226470b1a8c9585bf56ba7997ac102231b2
ccbien <chibien408@gmail.com>
2021-12-19 12:32:57 +0700

    4. sample test totally ok (fixed)

--- PS4/SRC/main.py
+++ PS4/SRC/main.py
@@ -18,7 +18,7 @@ def read_input(filepath):
 
 
 def PL_Resolution(clauses, filename):
-    f = open(os.path.join(OUTPUT_DIR, filename), 'w')
+    f = open(os.path.join(OUTPUT_DIR, filename), 'w') # File output
     clause_set = set(clauses)
     new_set = set()
     flag = False
@@ -30,31 +30,42 @@ def PL_Resolution(clauses, filename):
         for i in range(len(clauses)):
             for j in range(i + 1, len(clauses)):
                 resolvent = Clause.resolve(clauses[i], clauses[j]) # Hop giai
-                if resolvent is None or resolvent in new_set or resolvent in clauses:
-                    continue
-                if resolvent.is_empty():
-                    flag = True
+                if resolvent is None or resolvent in new_set or resolvent in clause_set:
+                    continue # Bo qua khi resolvent == None hoac resolvent da ton tai
+                if resolvent.is_empty(): 
+                    flag = True # Khi hop giai tra ve rong, danh dau flag de return khi ket thuc vong lap
                 new_set.add(resolvent)
                 lines.add(resolvent.to_string())
 
+        # Ghi ket qua vong lap vao file output
         f.write(str(len(lines)) + '\n')
         for line in lines:
             f.write(line + '\n')
+
+        # Neu flag duoc bat (tim ra resolvent rong) --> Ket luan la YES (KB entails alpha)
         if flag:
             f.write('YES')
             f.close()
             return
+
+        # Neu khong tim duoc clause mot --> Ket luan la NO (KB not entails alpha)
         if new_set.issubset(clause_set):
             f.write('NO')
             f.close()
             return
+
+        # Hop nhat hai set
         clause_set = clause_set.union(new_set)
 
 
 def main():
     for filename in os.listdir(INPUT_DIR):
         alpha, clauses = read_input(os.path.join(INPUT_DIR, filename))
-        clauses.extend(Clause.NOT(alpha))
+
+        # not_alpha la mot list chua cac clause cua NOT(alpha)
+        # VD: alpha = "A OR -B"  -->  NOT(alpha) = "-A AND B"  -->  ["-A", "B"]
+        not_alpha = Clause.NOT(alpha)
+        clauses.extend(not_alpha) # Them cac clause trong not_alpha vao danh sach
 
         PL_Resolution(clauses, filename)
 
